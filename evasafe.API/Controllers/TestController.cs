9b43540829b7fb4a6e61bd39df03dab363bf7a4b
sammyach <sammyach@gmail.com>
2022-07-01 14:58:12 +0000

    modified return value for hi method

--- evasafe.API/Controllers/TestController.cs
+++ evasafe.API/Controllers/TestController.cs
@@ -1,5 +1,4 @@
-﻿using evasafe.API.Data;
-using Microsoft.AspNetCore.Http;
+﻿using evasafe.API.data;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
@@ -19,17 +18,36 @@ namespace evasafe.API.Controllers
         [Route("hi")]
         public IActionResult Hi()
         {
-            return Ok($"Hi, API iz app and running @ {DateTime.UtcNow}");
+            string? rip = HttpContext.Connection?.RemoteIpAddress?.ToString(); //GetServerVariable("HTTP_X_FORWARDED_FOR");// .Current.Request.ServerVariables["HTTP_X_FORWARDED_FOR"];
+            string? lip = HttpContext.Connection?.LocalIpAddress?.ToString();
+            string? rip4 = HttpContext.Connection?.RemoteIpAddress?.MapToIPv4().ToString();
+
+            HttpContext.Request.Headers.TryGetValue("LocalTime", out var localTime);
+
+
+            //if (string.IsNullOrEmpty(ip))
+            //{
+            //    ip = HttpContext.GetServerVariable("REMOTE_ADDR"); //System.Web.HttpContext.Current.Request.ServerVariables["REMOTE_ADDR"];
+            //}
+            return Ok($"Hi, API iz app and running @ {DateTime.UtcNow.ToUniversalTime()} - remote: {rip} - remote4: {rip4} - local: {lip} - localTime: {localTime}");
         }
 
         [HttpGet]
         [Route("hi/db")]
         public async Task<IActionResult> PingDB()
         {
-            var db = _context.Database;
-            var mzg = db.CanConnect() ? "DB iz available and can be connected to." : "Cannot connect to DB";
-            var data = await _context.EvAccountActionsTypes.FirstOrDefaultAsync();
-            return Ok($"{DateTime.UtcNow}: {mzg} --- {data?.ActionType}");
+            try
+            {
+                var db = _context.Database;
+                var mzg = db.CanConnect() ? "DB iz available and can be connected to." : "Cannot connect to DB";
+                var data = await _context.EvAccountActionsTypes.FirstOrDefaultAsync();
+                return Ok($"{DateTime.UtcNow}: {mzg} --- {data?.ActionType}");
+            }
+            catch (Exception e)
+            {
+                return BadRequest($"xxx - {e.Message}");
+            }
+            
         }
 
         [HttpPost]
